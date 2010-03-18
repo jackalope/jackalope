f9f89c6aab37dde88ab31d4b211d55ea8781b53a
dbu <david.buchmann@liip.ch>
2010-03-18 17:49:55 +0100

    cleaned up tests for non-api classes

---
+++ tests/ImplementationHelpers/ObjectManager.php
@@ -0,0 +1,28 @@
+<?php
+require_once(dirname(__FILE__) . '/../inc/baseCase.php');
+
+class jackalope_tests_ObjectManager extends jackalope_baseCase {
+    private $JSON = '{":jcr:primaryType":"Name","jcr:primaryType":"rep:root","jcr:system":{},"tests_level1_access_base":{}}';
+    public function testGetNodeByPath() {
+        $path = '/jcr:root';
+        $om = new jackalope_ObjectManager($this->getTransportStub($path), $this->getSessionMock());
+        $node = $om->getNodeByPath($path);
+        $this->assertType('jackalope_Node', $node);
+        $this->assertEquals('rep:root', $node->getPrimaryNodeType());
+        $children = $this->getNodes();
+        $this->assertType('jackalope_NodeIterator', $children);
+        $this->assertEqual(2, $children->size());
+    }
+
+    private function getTransportStub($path) {
+        $transport = $this->getMock('jackalope_transport_DavexClient', array('getItem'), array('http://example.com'));
+        $transport->expects($this->once())
+            ->method('getItem')
+            ->with($this->equalTo($path))
+            ->will($this->returnValue(json_decode($this->JSON)));
+        return $transport;
+    }
+    private function getSessionMock() {
+        return $this->getMock('jackalope_Session', array(), array(), '', false);
+    }
+}
