e8c729e1ec7e949b79d3352aa15e07afd6182df1
Tobias Ebnöther <Ar@gorn.ch>
2010-03-22 17:57:06 +0100

    Added methods to identify an uuid and get an absolute path

--- src/jackalope/ObjectManager.php
+++ src/jackalope/ObjectManager.php
@@ -34,4 +34,43 @@ class jackalope_ObjectManager {
         }
         return $this->objectsByPath[$path];
     }
+    
+    /**
+     * Creates an absolute path from a root and an relative path
+     * @param string Root path to append the relative
+     * @param string Relative path…
+     * @return string Absolute path with . and .. resolved
+     */
+    protected function absolutePath($root, $relPath) {
+        $finalPath = array();
+        $path = array_merge(explode('/', '/' . $root), explode('/', '/' . $relPath));
+        foreach ($path as $pathPart) {
+            switch ($pathPart) {
+                case '.':
+                case '':
+                    break;
+                case '..':
+                    array_pop($finalPath);
+                    break;
+                default:
+                    array_push($finalPath, $pathPart);
+                    break;
+            }
+        }
+        return '/' . implode('/', $finalPath) . '/';
+    }
+    
+    /**
+     * Checks if the string could be a uuid
+     * @param string possible uuid
+     * @return bool if it looks like a uuid it will return true
+     */
+    protected function isUUID($id) {
+        // UUDID is HEX_CHAR{8}-HEX_CHAR{4}-HEX_CHAR{4}-HEX_CHAR{4}-HEX_CHAR{12}
+        if (1 === preg_match('/^[[:xdigit:]]{8}-[[:xdigit:]]{4}-[[:xdigit:]]{4}-[[:xdigit:]]{4}-[[:xdigit:]]{12}$/', $id)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- tests/ImplementationHelpers/ObjectManager.php
+++ tests/ImplementationHelpers/ObjectManager.php
@@ -1,6 +1,16 @@
 <?php
 require_once(dirname(__FILE__) . '/../inc/baseCase.php');
 
+class OMT extends jackalope_ObjectManager {
+    public function absolutePath($root, $relativePath) {
+        return parent::absolutePath($root, $relativePath);
+    }
+    
+    public function isUUID($i) {
+        return parent::isUUID($i);
+    }
+}
+
 class jackalope_tests_ObjectManager extends jackalope_baseCase {
     private $JSON = '{":jcr:primaryType":"Name","jcr:primaryType":"rep:root","jcr:system":{},"tests_level1_access_base":{}}';
     public function testGetNodeByPath() {
@@ -22,5 +32,33 @@ class jackalope_tests_ObjectManager extends jackalope_baseCase {
     }
     private function getSessionMock() {
         return $this->getMock('jackalope_Session', array(), array(), '', false);
+    }
+    
+    public function testIsUUID() {
+        $om = new OMT($this->getTransportStub('/jcr:root'), $this->getSessionMock());
+        $this->assertFalse($om->isUUID(''));
+        $this->assertFalse($om->isUUID('/'));
+        $this->assertFalse($om->isUUID('/foo'));
+        $this->assertFalse($om->isUUID('../foo'));
+        $this->assertFalse($om->isUUID('./foo'));
+        $this->assertFalse($om->isUUID('sdfsafd'));
+        $this->assertFalse($om->isUUID('842e61c0-09aba42a9-87c0-308ccc90e6f4'));
+        $this->assertFalse($om->isUUID('z42e61c0-09ab-a42a9-87c0-308ccc90e6f4'));
+        $this->assertFalse($om->isUUID('842e61c0a-09ab-a42a9-87c0-308ccc90e6f4'));
+        $this->assertTrue($om->isUUID('842e61c0-09ab-42a9-87c0-308ccc90e6f4'));
+        $this->assertTrue($om->isUUID('842E61C0-09AB-A42a-87c0-308ccc90e6f4'));
+    }
+    
+    public function testAbsolutePath() {
+        $om = new OMT($this->getTransportStub('/jcr:root'), $this->getSessionMock());
+        $this->assertEquals('/jcr:root/', $om->absolutePath('/', 'jcr:root'));
+        $this->assertEquals('/jcr:root/', $om->absolutePath('/', './jcr:root'));
+        $this->assertEquals('/jcr:root/', $om->absolutePath('/jcr:root', ''));
+        $this->assertEquals('/jcr:root/foo_/b-a/0^/', $om->absolutePath('jcr:root', 'foo_/b-a/0^/'));
+        $this->assertEquals('/jcr:root/foo_/b-a/0^/', $om->absolutePath('/jcr:root', '/foo_/b-a/0^/'));
+        $this->assertEquals('/jcr:root/foo_/b-a/0^/', $om->absolutePath('jcr:root/', '/foo_/b-a/0^'));
+        $this->assertEquals('/jcr:root/foo/bar/', $om->absolutePath('/jcr:root/wrong/', '../foo/bar/'));
+        $this->assertEquals('/jcr:root/foo/bar/', $om->absolutePath('/jcr:root/wrong/', '/../foo/bar/'));
+        $this->assertEquals('/jcr:root/foo/bar/', $om->absolutePath('/jcr:root/wrong/', '/foo/../../foo/bar/'));
     }
 }
