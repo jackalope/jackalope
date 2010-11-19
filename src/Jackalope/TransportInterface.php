46f480e6ace44f3ad6703feabdbcf5ef0e9316db
Bastian Feder <lapis@bastian-feder.de>
2010-11-19 16:42:00 +0100

    added a new method since it is already used in DavexCleint

--- src/Jackalope/TransportInterface.php
+++ src/Jackalope/TransportInterface.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Definition of the interface to be used for implementing a transport.
+ *
+ * @license http://www.apache.org/licenses Apache License Version 2.0, January 2004
+ *
+ * @package jackalope
+ * @subpackage transport
+ */
 namespace Jackalope;
 
 /**
@@ -9,16 +17,21 @@ namespace Jackalope;
  * Look in the transport/ subfolder for actual implementations.
  *
  * TODO: THIS INTERFACE IS INCOMPLETE! get all methods that are really needed from DavexClient
+ *
+ * @package jackalope
+ * @subpackage transport
  */
 interface TransportInterface {
     /**
      * Set this transport to a specific credential and a workspace.
+     *
      * This can only be called once. To connect to another workspace or with
      * another credential, use a fresh instance of transport.
      *
      * @param credentials A \PHPCR\SimpleCredentials instance (this is the only type currently understood)
      * @param workspaceName The workspace name for this transport.
      * @return true on success (exceptions on failure)
+     *
      * @throws \PHPCR\LoginException if authentication or authorization (for the specified workspace) fails
      * @throws \PHPCR\NoSuchWorkspacexception if the specified workspaceName is not recognized
      * @throws \PHPCR\RepositoryException if another error occurs
@@ -33,5 +46,14 @@ interface TransportInterface {
      * @throws \PHPCR\RepositoryException if error occurs
      */
     public function getRepositoryDescriptors();
+
+    /**
+     * Get the registered namespaces mappings from the backend.
+     *
+     * @return array Associative array of prefix => uri
+     *
+     * @throws \PHPCR\RepositoryException if now logged in
+     */
+    public function getNamespaces();
 }
 
