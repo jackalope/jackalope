91222d19739556ff0c141fe68edb141aa9dc92b7
Bastian Feder <lapis@bastian-feder.de>
2010-11-21 14:22:11 +0100

    adaptions made due to the introduction of the proxy generator

--- src/Jackalope/Transport/DavexClient/Request.php
+++ src/Jackalope/Transport/DavexClient/Request.php
@@ -2,6 +2,19 @@
 /**
  * Class to handle Davex requests.
  *
+ * @license http://www.apache.org/licenses/LICENSE-2.0  Apache License Version 2.0, January 2004
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
  * @package jackalope
  * @subpackage transport
  */
@@ -17,10 +30,10 @@ namespace Jackalope\Transport\DavexClient;
 class Request {
 
     /**
-     * Identifier of the 'GET' http request method.
-     * @var string
+     * DOM representation of the request.
+     * @var DOMDocument
      */
-    const GET = 'GET';
+    protected $dom = null;
 
     /**
      * Identifier of the 'REPORT' http request method.
@@ -40,11 +53,6 @@ class Request {
      */
     const PROPPATCH = 'PROPPATCH';
 
-    /**
-     * DOM representation of the request.
-     * @var DOMDocument
-     */
-    protected $dom = null;
 
     /*************************************************************************/
     /* Depedencies
@@ -57,11 +65,10 @@ class Request {
      */
     protected function getDomObject() {
         if ($this->dom === NULL) {
-            $this->dom = new \DOMDocument();
+            $this->dom = new \DOMDocument('1.0', 'UTF-8');
         }
         return $this->dom;
     }
-
 
     /*************************************************************************/
     /* magic methods
--- tests/Jackalope/Transport/DavexClient/RequestTest.php
+++ tests/Jackalope/Transport/DavexClient/RequestTest.php
@@ -1,22 +1,38 @@
 <?php
 
-namespace  Jackalope\Transport\DavexClient;
+namespace Jackalope\Transport\DavexClient;
 
 class RequestTest extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * Provides a proxy of the \Jackalope\Transport\DavexClient\Request class.
+     *
+     * @param array $methods
+     * @param mixed $type
+     *
+     * @return \Jackalope\Transport\DavexClient\Request Proxy instance exposing invisible methods.
+     */
+    public function getRequestProxy($methods, $type) {
+        if (!is_array($methods)) {
+            $methods = array($methods);
+        }
+
+        $proxyGenerator = new \Tests\Unittests\ProxyObject;
+        return $proxyGenerator->getProxy(
+            '\Jackalope\Transport\DavexClient\Request',
+            $methods,
+            array($type),
+            '',
+            true
+        );
+    }
 
     /**
      * @covers  \Jackalope\Transport\DavexClient\Request::getDomObject
      */
     public function testGetDomObject() {
-        $request = new RequestProxy();
+        $request = $this->getRequestProxy('getDomObject', '');
         $request->getDomObject();
         $this->assertAttributeInstanceOf('DOMDocument', 'dom', $request);
-    }
-}
-
-class RequestProxy extends \Jackalope\Transport\DavexClient\Request {
-
-    public function getDomObject() {
-        return parent::getDomObject();
     }
 }
