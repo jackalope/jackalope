1cfc3e26d7607ae908f8b2bf32bfbfcd8f34f52d
Bastian Feder <lapis@bastian-feder.de>
2010-11-21 18:30:27 +0100

    imcomplete version of the request interface

---
+++ src/Jackalope/Interfaces/DavexClient/Request.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Jackalope\Interfaces\DavexClient;
+
+interface Request {
+
+    /**
+     * Identifier of the 'GET' http request method.
+     * @var string
+     */
+    const GET = 'GET';
+
+    /**
+     * Identifier of the 'REPORT' http request method.
+     * @var string
+     */
+    const REPORT = 'REPORT';
+
+    /**
+     * Identifier of the 'PROPFIND' http request method.
+     * @var string
+     */
+    const PROPFIND = 'PROPFIND';
+
+    /**
+     * Identifier of the 'PROPPATCH' http request method.
+     * @var string
+     */
+    const PROPPATCH = 'PROPPATCH';
+
+    /**
+     * Generates the DOMDocument representing the request.
+     * @return null
+     */
+    public function build();
+}
