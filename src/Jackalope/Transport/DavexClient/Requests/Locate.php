b642ac341a52ce562b90d498ca3b92e5dc631359
Bastian Feder <lapis@bastian-feder.de>
2010-11-22 11:26:57 +0100

    implemented Locate request

---
+++ src/Jackalope/Transport/DavexClient/Requests/Locate.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Class to handle the communication between Jackalope and Jackrabbit via Davex.
+ *
+ * @license http://www.apache.org/licenses/LICENSE-2.0  Apache License Version 2.0, January 2004
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ * @package jackalope
+ * @subpackage transport
+ */
+
+namespace Jackalope\Transport\DavexClient\Requests;
+
+/**
+ *
+ */
+class Locate implements \Jackalope\Interfaces\DavexClient\Request {
+
+
+    /**
+     * DOM representation of the request.
+     * @var DOMDocument
+     */
+    protected $dom = null;
+
+    /**
+     * List of arguments to be handled.
+     * @var array
+     */
+    protected $arguments = array();
+
+    /**
+     * Identifier of the used XML namespace
+     * @var array
+     */
+    protected $nsInformation = array(
+        'D'   => 'DAV:',
+        'dcr' => 'http://www.day.com/jcr/webdav/1.0'
+    );
+
+    /**
+     * Initiaties the NodeTypes request object.
+     *
+     * @param DOMDocument $dom
+     * @param array $arguments
+     */
+    public function __construct($dom, array $arguments) {
+        $this->dom = $dom;
+        $this->arguments = $arguments;
+    }
+
+    /**
+     * Generates the DOMDocument representing the request to be send.
+     *
+     * Available properties:
+     *  - D:workspace
+     *  - dcr:workspaceName
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function build() {
+
+        if (empty($this->arguments['uuid'])) {
+            throw new \InvalidArgumentException('Missing UUID in argument list.');
+        }
+
+        $doc = $this->dom->createElementNS($this->nsInformation['dcr'], 'dcr:locate-by-uuid');
+        $href = $this->dom->createElementNS($this->nsInformation['D'], 'D:href', $this->arguments['uuid']);
+        $doc->appendChild($href);
+        $this->dom->appendChild($doc);
+    }
+
+    /**
+     * Generate the XML string from the created DOMDocument.
+     *
+     * @return string The XML string representation of the recent generated DOMDocument.
+     */
+    public function getXml() {
+        return strval($this);
+    }
+
+    /*************************************************************************/
+    /* Magic methods
+    /*************************************************************************/
+
+    /**
+     * Generate the XML string from the created DOMDocument.
+     *
+     * @return string The XML string representation of the recent generated DOMDocument.
+     */
+    public function __toString() {
+        return $this->dom->saveXML();
+    }
+}
---
+++ tests/Jackalope/Transport/DavexClient/Requests/LocateTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Jackalope\Transport\DavexClient\Requests;
+
+class LocateTest extends \PHPUnit_Framework_TestCase {
+
+    /*************************************************************************/
+    /* Fixtures
+    /*************************************************************************/
+
+    /**
+     * Generates the absolute path to a fixture file.
+     *
+     * @param string $filename Name of the file the path shall be generated for.
+     * @return string The location of the file.
+     */
+    public function getFixtureFile($filename) {
+        $path = __DIR__.'/../../../../fixtures/Requests/';
+        return $path.$filename;
+    }
+
+    /**
+     * Provides an instance of the \Jackalope\Transport\DavexClient\Requests\Locate class.
+     *
+     * @param array $arguments List of arguments to be processed.
+     * @return \Jackalope\Transport\DavexClient\Requests\Locate
+     */
+    public function getLocateObject($arguments) {
+        $dom = new \DOMDocument('1.0', 'UTF-8');
+        $dom->formatOutput = true;
+        return new \Jackalope\Transport\DavexClient\Requests\Locate($dom, $arguments);
+    }
+
+    /*************************************************************************/
+    /* Tests
+    /*************************************************************************/
+
+    /**
+     * @covers \Jackalope\Transport\DavexClient\Requests\Locate::__construct
+     */
+    public function testConstruct() {
+        $arguments = array('properties' => array('D:workspace', 'dcr:workspaceName'));
+        $request = $this->getLocateObject($arguments);
+
+        $this->assertAttributeEquals($arguments, 'arguments', $request);
+        $this->assertAttributeInstanceOf('DOMDocument', 'dom', $request);
+    }
+
+    /**
+     * @covers \Jackalope\Transport\DavexClient\Requests\Locate::getXml
+     */
+    public function testGetXml() {
+        $arguments = array('uuid' => 'b58401da57c82346c4d3c01e1509a4b861a55114');
+        $request = $this->getLocateObject($arguments);
+        $request->build();
+
+        $this->assertXmlStringEqualsXmlFile(
+            $this->getFixtureFile('LocateBuildWithOneUuid.xml'),
+            $request->getXml()
+        );
+    }
+
+    /**
+     * @covers \Jackalope\Transport\DavexClient\Requests\Locate::build
+     * @covers \Jackalope\Transport\DavexClient\Requests\Locate::__toString
+     */
+    public function testBuild() {
+        $arguments = array('uuid' => 'b58401da57c82346c4d3c01e1509a4b861a55114');
+        $request = $this->getLocateObject($arguments);
+        $request->build();
+        $this->assertXmlStringEqualsXmlFile(
+            $this->getFixtureFile('LocateBuildWithOneUuid.xml'),
+            strval($request)
+        );
+    }
+
+    /**
+     * @covers \Jackalope\Transport\DavexClient\Requests\Locate::build
+     * @expectedException \InvalidArgumentException
+     */
+    public function testBuildExpectingInvalidArgumentException() {
+        $request = $this->getLocateObject(array());
+        $request->build();
+    }
+}
