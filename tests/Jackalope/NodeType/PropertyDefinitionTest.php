46a7b15128da4ff620ab6b7f88715550a0ae219b
Benjamin Eberlei <kontakt@beberlei.de>
2011-05-07 23:51:35 +0200

    Add tests for Jackalope\NodeType\PropertyType

--- tests/Jackalope/NodeType/PropertyDefinitionTest.php
+++ tests/Jackalope/NodeType/PropertyDefinitionTest.php
@@ -3,13 +3,54 @@
 namespace Jackalope\NodeType;
 
 use Jackalope\TestCase;
+use PHPCR\Version\OnParentVersionAction;
 
 class PropertyDefinitionTest extends TestCase
 {
-    public function testDummy()
+    private $defaultData = array(
+        'declaringNodeType' => 'nt:unstructured',
+        'name' => 'foo',
+        'isAutoCreated' => false,
+        'mandatory' => false,
+        'isProtected' => false,
+        'onParentVersion' => "COPY",
+        'requiredType' => 'binary',
+        'multiple' => false,
+        'fullTextSearchable' => false,
+        'queryOrderable' => false,
+        'valueConstraints' => array(),
+        'availableQueryOperators' => array(),
+        'defaultValues' => '',
+    );
+
+    public function testCreateFromArray()
     {
-        $this->markTestSkipped('No tests for this class yet');
+        $factory = $this->getMock('Jackalope\Factory');
+        $nodeTypeManager = $this->getMock('Jackalope\NodeType\NodeTypeManager', array(), array(), '', false);
+        $propType = new PropertyDefinition($factory, $this->defaultData, $nodeTypeManager);
+
+        $this->assertEquals('foo', $propType->getName());
+        $this->assertFalse($propType->isAutoCreated());
+        $this->assertFalse($propType->isMandatory());
+        $this->assertFalse($propType->isProtected());
+        $this->assertEquals(OnParentVersionAction::COPY, $propType->getOnParentVersion());
+        $this->assertEquals('binary', $propType->getRequiredType());
+        $this->assertFalse($propType->isMultiple());
+        $this->assertFalse($propType->isQueryOrderable());
+        $this->assertFalse($propType->isFullTextSearchable());
     }
 
-    
+    public function testGetDeclaringNodeType()
+    {
+        $nodeType = $this->getMock('Jackalope\NodeType\NodeTypeDefinition', array(), array(), '', false);
+        $factory = $this->getMock('Jackalope\Factory');
+        $nodeTypeManager = $this->getMock('Jackalope\NodeType\NodeTypeManager', array(), array(), '', false);
+        $nodeTypeManager->expects($this->once())
+                        ->method('getNodeType')
+                        ->with($this->equalTo('nt:unstructured'))
+                        ->will($this->returnValue($nodeType));
+        $propType = new PropertyDefinition($factory, $this->defaultData, $nodeTypeManager);
+
+        $this->assertSame($nodeType, $propType->getDeclaringNodeType());
+    }
 }
