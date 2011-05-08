59fe1cf3e73881f96f3de84c3eeb5505cec12ac5
Lukas Kahwe Smith <smith@pooteeweet.org>
2011-05-08 15:53:44 +0200

    typo fix in phpdoc

--- src/Jackalope/Item.php
+++ src/Jackalope/Item.php
@@ -68,7 +68,7 @@ class Item implements \PHPCR\ItemInterface
     /**
      * Returns the normalized absolute path to this item.
      *
-     * @returns string the normalized absolute path of this Item.
+     * @return string the normalized absolute path of this Item.
      * @throws \PHPCR\RepositoryException if an error occurs.
      * @api
      */
