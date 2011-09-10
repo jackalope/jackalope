jackalope/jackalope

commit 1603a98a0ab8dfe42b3406d7f2ede0e5d52d6521

fixed a bug in type handling in Node::parseData()

src/Jackalope/Node.php
@@ -63,7 +63,7 @@ class Node extends Item implements \IteratorAggregate, \PHPCR\NodeInterface
      * @see Node::__construct()
      * @see Node::refresh()
      */
-    private function parseData($rawData, $update, $keepChanges=false)
+    private function parseData($rawData, $update, $keepChanges = false)
     {
         //TODO: refactor to use hash array instead of stdClass struct
 
@@ -165,9 +165,9 @@ class Node extends Item implements \IteratorAggregate, \PHPCR\NodeInterface
 
                     // OPTIMIZE: do not instantiate properties until needed
                     default:
-                        $type = isset($rawData->{':' . $key}) ?
-                                    PropertyType::valueFromName($rawData->{':' . $key}) :
-                                    PropertyType::determineType(is_array($value) ? reset($value) : $value);
+                        $type = isset($rawData->{':' . $key})
+                                ? $rawData->{':' . $key}
+                                : PropertyType::determineType(is_array($value) ? reset($value) : $value);
                         $this->_setProperty($key, $value, $type, true);
                         break;
                 }

commit 9c78371a05af895aa9215a43e2ad84b0cf6cf437

fix bug with jackrabbit transport introduced in 1603a98a0ab8dfe42b3406d7f2ede0e5d52d6521

src/Jackalope/Node.php
@@ -185,9 +185,22 @@ class Node extends Item implements \IteratorAggregate, \PHPCR\NodeInterface
 
                     // OPTIMIZE: do not instantiate properties until needed
                     default:
-                        $type = isset($rawData->{':' . $key})
-                                ? $rawData->{':' . $key}
-                                : PropertyType::determineType(is_array($value) ? reset($value) : $value);
+                        if (isset($rawData->{':' . $key})) {
+                            /*
+                             * this is an inconsistency between jackrabbit and
+                             * dbal transport: jackrabbit has type name, dbal
+                             * delivers numeric type.
+                             * we should eventually fix the format returned by
+                             * transport and either have jackrabbit transport
+                             * do the conversion or let dbal store a string
+                             * value instead of numerical.
+                             */
+                            $type = is_numeric($rawData->{':' . $key})
+                                    ? $rawData->{':' . $key}
+                                    : PropertyType::valueFromName($rawData->{':' . $key});
+                        } else {
+                            $type = PropertyType::determineType(is_array($value) ? reset($value) : $value);
+                        }
                         $this->_setProperty($key, $value, $type, true);
                         break;
                 }