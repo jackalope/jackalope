fa1541d77391a8708e57eb10e5fedc9f8e469b3f
dbu <david.buchmann@liip.ch>
2011-11-23 10:07:52 +0100

    generate doctrine fixtures: include the uuid helper with the right path. fixes #40

--- tests/generate_doctrine_dbal_fixture.php
+++ tests/generate_doctrine_dbal_fixture.php
@@ -6,7 +6,7 @@
  * @author Benjamin Eberlei <kontakt@beberlei.de>
  */
 
-require_once __DIR__ . "/../lib/phpcr/src/PHPCR/Util/UUIDHelper.php";
+require_once __DIR__ . "/../lib/phpcr-utils/src/PHPCR/Util/UUIDHelper.php";
 
 $srcDir = __DIR__ . "/phpcr-api/fixtures";
 $destDir = __DIR__ . "/fixtures/doctrine";
