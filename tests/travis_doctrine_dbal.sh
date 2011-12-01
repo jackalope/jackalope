4d00caf2101e29c588f1d12494e96818691df566
lsmith77 <smith@pooteeweet.org>
2011-12-01 19:52:15 +0100

    create the test db

--- tests/travis_doctrine_dbal.sh
+++ tests/travis_doctrine_dbal.sh
@@ -4,6 +4,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 git submodule update --init --recursive
 
+mysql -e 'create database IF NOT EXISTS phpcr_tests;'
+
 php $DIR/generate_doctrine_dbal_fixture.php
 
 php $DIR/vendor/vendors_doctrine_dbal.php
