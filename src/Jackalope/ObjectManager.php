643c4b057722373d90f0afe709410b3dcff8bd53
Christian Stocker <me@chregu.tv>
2012-03-05 13:54:18 +0100

    Added ObjectManager->finishSave() to be called after all save operations are done Added 3rd parameter to ObjectManager->moveNode() to move node immediately
    Needed for JSOP support

--- src/Jackalope/ObjectManager.php
+++ src/Jackalope/ObjectManager.php
@@ -793,6 +793,11 @@ class ObjectManager
                 }
             }
         }
+        
+        if (method_exists($this->transport, "finishSave")) {
+            $this->transport->finishSave();
+        }
+
 
         //clear those lists before reloading the newly added nodes from backend, to avoid collisions
         $this->itemsRemove = array();
@@ -1252,7 +1257,7 @@ class ObjectManager
         $this->verifyAbsolutePath($srcAbsPath);
         $this->verifyAbsolutePath($destAbsPath);
 
-        $this->transport->moveNode($srcAbsPath, $destAbsPath); // should throw the right exceptions
+        $this->transport->moveNode($srcAbsPath, $destAbsPath, true); // should throw the right exceptions
         $this->rewriteItemPaths($srcAbsPath, $destAbsPath); // update local cache
     }
 
