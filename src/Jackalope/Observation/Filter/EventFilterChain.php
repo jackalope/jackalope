0ea03631e46c3dd90ba3c9238232a00a7cc79200
Dan <dan@dc-home.ch>
2012-03-18 19:19:55 +0100

    Added code to extract the event journal filtering from the event journal.
    Needs a review !

---
+++ src/Jackalope/Observation/Filter/EventFilterChain.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Jackalope\Observation\Filter;
+
+use PHPCR\Observation\EventInterface;
+
+/**
+ * Event filter that execute a list of other event filters. It stops the chain
+ * as soon as a filter rejects an event. Thus it can be used to AND event
+ * filters.
+ */
+class EventFilterChain implements EventFilterInterface
+{
+    protected $filters;
+
+
+    public function __construct()
+    {
+        $this->filters = array();
+    }
+
+    /**
+     * Factory method to construct a EventFilterChain from the parameters given to ObservationManager::getJournal
+     *
+     * @static
+     * @param \PHPCR\SessionInterface $session
+     * @param int|null $eventTypes
+     * @param string|null $absPath
+     * @param boolean|null $isDeep
+     * @param array|null $uuid
+     * @param array|null $nodeTypeName
+     * @return EventFilterChain
+     */
+    public static function constructFilterChain(\PHPCR\SessionInterface $session, $eventTypes = null, $absPath = null, $isDeep = null, array $uuid = null, array $nodeTypeName = null)
+    {
+        $filter = new EventFilterChain();
+
+        if ($eventTypes) {
+            $filter->addFilter(new EventTypeEventFilter($eventTypes));
+        }
+
+        if ($absPath) {
+            $filter->addFilter(new PathEventFilter($absPath, $isDeep));
+        }
+
+        if ($uuid) {
+            $filter->addFilter(new UuidEventFilter($session, $uuid));
+        }
+
+        if ($nodeTypeName) {
+            // TODO: create a new TimeNameFilter and add it to the chain
+        }
+
+        return $filter;
+    }
+
+    /**
+     * Add a filter to the filter chain
+     * @param EventFilterInterface $filter
+     * @return void
+     */
+    public function addFilter(EventFilterInterface $filter)
+    {
+        $this->filters[] = $filter;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function match(EventInterface $event)
+    {
+        foreach ($this->filters as $filter)
+        {
+            if (!$filter->filter($event)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ tests/Jackalope/Observation/UuidEventFilterTest.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace Jackalope\Observation;
+
+use Jackalope\TestCase,
+    PHPCR\Observation\EventInterface,
+    Jackalope\Observation\Event,
+    Jackalope\Observation\Filter\UuidEventFilter;
+
+
+/**
+ * Unit tests for the EventJournal
+ */
+class UuidEventFilterTest extends TestCase
+{
+    public function setUp()
+    {
+    }
+
+    public function testFilter()
+    {
+        $filter = $this->getFilter(array('1', '2', '3'));
+        $this->assertTrue($filter->match($this->getEvent('/1/2')));
+        $this->assertTrue($filter->match($this->getEvent('/2/2')));
+        $this->assertTrue($filter->match($this->getEvent('/3/2')));
+        $this->assertFalse($filter->match($this->getEvent('/1/2/3')));
+        $this->assertFalse($filter->match($this->getEvent('/4/1')));
+        $this->assertFalse($filter->match($this->getEvent('/1')));
+    }
+
+    public function testNoMatchFilter()
+    {
+        $filter = $this->getFilter(array());
+        $this->assertFalse($filter->match($this->getEvent('/1/2')));
+        $this->assertFalse($filter->match($this->getEvent('/2/2')));
+        $this->assertFalse($filter->match($this->getEvent('/3/2')));
+        $this->assertFalse($filter->match($this->getEvent('/1/2/3')));
+        $this->assertFalse($filter->match($this->getEvent('/4/1')));
+        $this->assertFalse($filter->match($this->getEvent('/1')));
+    }
+
+    /**
+     * Get an Event with the given path
+     * @param string $path
+     * @return Event
+     */
+    protected function getEvent($path)
+    {
+        $event = new Event();
+        $event->setPath($path);
+        return $event;
+    }
+
+    /**
+     * Get a UuidEventFilter matching the given UUIDs.
+     * Additionally set the session getNodesByIdentifier to return an array of nodes
+     * that will match the requirements of the filter
+     * @param $uuids
+     * @return Filter\UuidEventFilter
+     */
+    protected function getFilter($uuids)
+    {
+        $nodes = array();
+        foreach ($uuids as $uuid) {
+            $nodes[] = $this->getNodeMock($uuid);
+        }
+        return new UuidEventFilter($this->getMySessionMock($nodes), $uuids);
+    }
+
+    public function getMySessionMock($nodesByIdentifier)
+    {
+        $factory = new \Jackalope\Factory;
+        $session = $this->getMock('\Jackalope\Session', array('getWorkspace', 'getRepository', 'getNodesByIdentifier'), array($factory), '', false);
+        $session
+            ->expects($this->any())
+            ->method('getWorkspace')
+            ->will($this->returnValue($this->getWorkspaceMock()));
+        $session
+            ->expects($this->any())
+            ->method('getRepository')
+            ->will($this->returnValue($this->getRepositoryMock()));
+        $session
+            ->expects($this->any())
+            ->method('getNodesByIdentifier')
+            ->will($this->returnValue($nodesByIdentifier));
+        return $session;
+    }
+
+    /**
+     * Get a Jackalope\Node mock object that will return /uuid as path
+     * @param string $uuid
+     * @return object
+     */
+    protected function getNodeMock($uuid)
+    {
+        $node = $this->getMock(
+            '\Jackalope\Node',
+            array('getPath'),
+            array(new \Jackalope\Factory(), array(), '', $this->getSessionMock(), $this->getObjectManagerMock())
+        );
+        $node
+            ->expects($this->any())
+            ->method('getPath')
+            ->will($this->returnValue('/' . $uuid));
+
+        return $node;
+    }
+}
