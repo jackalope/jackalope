52e0ef8613394724ac62aa026d6e5bf0330a8a45
Daniel Barsotti <daniel.barsotti@liip.ch>
2012-03-19 10:50:44 +0100

    First naive implementation of Observation\Filter\NodeTypeEventFilter

---
+++ src/Jackalope/Observation/Filter/NodeTypeEventFilter.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Jackalope\Observation\Filter;
+
+use PHPCR\SessionInterface,
+    PHPCR\Observation\EventInterface;
+
+/**
+ * Naive implementation of the event filter based on the node type of the
+ * node attached to the event. This implementation does not take care of
+ * optimizations.
+ *
+ * TODO: subclass this and optimize
+ */
+class NodeTypeEventFilter implements EventFilterInterface
+{
+    /**
+     * @var \PHPCR\SessionInterface
+     */
+    protected $session;
+
+    /**
+     * @var array
+     */
+    protected $nodeTypes;
+
+    /**
+     * @param array $nodeTypes
+     */
+    public function __construct(SessionInterface $session, $nodeTypes)
+    {
+        $this->session = $session;
+        $this->nodeTypes = $nodeTypes;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function match(EventInterface $event)
+    {
+        $node = $this->session->getNode($event->getPath());
+        foreach ($this->nodeTypes as $type) {
+            if ($node->isNodeType($type)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ tests/Jackalope/Observation/NodeTypeEventFilterTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Jackalope\Observation;
+
+use Jackalope\TestCase,
+    PHPCR\Observation\EventInterface,
+    Jackalope\Observation\Event,
+    Jackalope\Observation\Filter\NodeTypeEventFilter;
+
+
+/**
+ * Unit tests for the EventJournal
+ */
+class NodeTypeEventFilterTest extends TestCase
+{
+    public function testFilter()
+    {
+        $filter = $this->getFilter($this->getMyNodeMock(true), array('nt:unstructured'));
+        $this->assertFilterMatch($filter, true);
+
+        $filter = $this->getFilter($this->getMyNodeMock(false), array('nt:unstructured'));
+        $this->assertFilterMatch($filter, false);
+    }
+
+    protected function assertFilterMatch(NodeTypeEventFilter $filter, $expectedResult)
+    {
+        $this->assertEquals($expectedResult, $filter->match(new Event()));
+    }
+
+    protected function getFilter($node, $nodeTypes)
+    {
+        return new NodeTypeEventFilter($this->getMySessionMock($node), $nodeTypes);
+    }
+
+    /**
+     * Returns a mock object for the Session.
+     * @param \PHPCR\NodeInterface $node The node returned by getNode
+     * @return \Jakalope\Session
+     */
+    public function getMySessionMock($node)
+    {
+        $session = $this->getSessionMock(array('getNode'));
+        $session
+            ->expects($this->any())
+            ->method('getNode')
+            ->will($this->returnValue($node));
+        return $session;
+    }
+
+    /**
+     * Get a Jackalope\Node mock object that will return $isNodeTypeResult when
+     * isNodeType is called on it.
+     * @param string $isNodeTypeResult
+     * @return object
+     */
+    protected function getMyNodeMock($isNodeTypeResult)
+    {
+        $node = $this->getNodeMock(array('isNodeType'));
+        $node
+            ->expects($this->any())
+            ->method('isNodeType')
+            ->will($this->returnValue($isNodeTypeResult));
+        return $node;
+    }
+
+}
