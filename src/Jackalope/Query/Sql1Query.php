b8f32efac0ec62e225d200d4bcc7e676465576b5
Christian Stocker <me@chregu.tv>
2012-04-12 10:42:45 +0200

    Add SQL(1) Support

---
+++ src/Jackalope/Query/Sql1Query.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Jackalope\Query;
+
+/**
+ * Query implementation for the (deprecated) SQL(1) language
+ *
+ * This can never be legally created if the transport does not implement
+ * QueryInterface
+ */
+class Sql1Query extends Query
+{
+
+    /**
+     * Access the query statement from the transport layer
+     *
+     * @return string the sql1 query statement
+     *
+     * @private
+     */
+    public function getStatementSql()
+    {
+        return $this->getStatement();
+        //TODO: should this expand bind variables? or the transport?
+    }
+
+    /**
+     * Returns the constant QueryInterface::JCR-SQL
+     *
+     * @return string the query language.
+     */
+    public function getLanguage()
+    {
+       return self::JCR_SQL;
+    }
+
+}
