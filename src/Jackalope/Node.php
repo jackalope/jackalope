20ead1a92fe9ed8a0ee3fd93335428992f3de7ca
David Buchmann <david.buchmann@liip.ch>
2013-01-28 15:13:46 +0100

    use a default namespace in addNodeAutoName that is existing for sure

--- src/Jackalope/Node.php
+++ src/Jackalope/Node.php
@@ -416,7 +416,7 @@ class Node extends Item implements IteratorAggregate, NodeInterface
         $name = NodeHelper::generateAutoNodeName(
             $this->nodes,
             $this->session->getWorkspace()->getNamespaceRegistry()->getNamespaces(),
-            'phpcr',
+            'jcr',
             $nameHint
         );
         return $this->addNode($name, $primaryNodeTypeName);
